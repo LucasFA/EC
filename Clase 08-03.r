6ca130e53b762c65dea72671e270e745104cf1bf
LucasFA <lucaslanzarote@gmail.com>
2022-03-08 12:24:36 +0100

    Terminada clase 08-03

--- Clase 08-03.r
+++ Clase 08-03.r
@@ -66,3 +66,72 @@ rbind(x, 51:55)
 A <- matrix(1:9, 3, 3)
 rownames(A) <- c("fila.1", "fila.2", "fila.3")
 A
+class(A)
+attributes(A)
+
+
+A <- matrix(1:9, 3, 3)
+
+A[, 1]
+A[, -1]
+A[-1, 1]
+A[-1, -1]
+A[1:2, 2:3]
+A > 3
+
+B <- A
+B[A > 3] <- NA
+B
+
+# Ejercicio
+# Matrix cuadrada dimension 3 runif
+set.seed(1)
+A <- matrix(runif(9), 3, 3)
+# i)
+matriz_bools <- A >= 0.5
+matriz_bools
+A[matriz_bools]
+# ii)
+sum(matriz_bools)
+# iii)
+B <- A
+B
+B[!matriz_bools] <- 0
+B
+# iv)
+C <- A
+C[col(A) < row(A)] <- NA
+
+D <- A
+D[col(A) > row(A)] <- NA
+D
+
+
+# Arrays
+x <- 1:24
+dim(x) <- c(3, 4, 2)
+x
+x <- array(1:24, dim = c(3, 4, 2))
+attributes(x)
+x[, , ]
+
+# Operaciones con matrices
+A <- matrix(c(0, 1, -1, 2), 2, 2)
+A
+# * es elemento a elemento
+A * A
+
+A %*% A
+crossprod(A, A)
+A <- matrix(c(3, 5, 2, 4, -2, -2, -1, 1, 1), 3, 3)
+b <- c(8, 4, 1)
+solve(A, b)
+
+AA <- crossprod(A, A)
+solve(AA)
+chol2inv(chol(AA))
+
+f <- function(x, y) 2 * y / (1 + x^2)
+xx <- seq(0, 1, length.out = 5)
+yy <- xx + 1
+outer(xx, yy, f)
