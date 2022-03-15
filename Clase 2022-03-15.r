1b190af37ad32974a26348c5b2ca1198c54fc80a
LucasFA <lucaslanzarote@gmail.com>
2022-03-15 11:05:09 +0100

    Clase 2022-03-15
    Añadido lo que tenía al salir

---
+++ Clase 2022-03-15.r
@@ -0,0 +1,73 @@
+# Clase 2022-03-15
+# Listas
+rm(list = ls())
+A <- matrix(1:6, 3, 2)
+m1 <- rowMeans(A)
+m2 <- colMeans(A)
+minA <- min(A)
+maxA <- max(A)
+nomb <- c("col1", "col2", "col3")
+cond <- A > mean(A) + sd(A)
+cond
+A
+lista <- list(A, m1, m2, minA, maxA, nomb, cond)
+lista
+
+lista <- list(
+    matriz = A, med_fil = m1, med_col = m2, minimo = minA, maximo = maxA,
+    nombres = nomb, cond = cond
+)
+lista[["matriz"]]
+lista$matriz
+
+class(lista)
+lapply(lista, class)
+lapply(lista, mean)
+
+sapply(lista, class)
+sapply(lista, mean)
+
+unlist(lista)
+
+class(lista[1])
+class(lista[[1]])
+
+x <- rnorm(100)
+res <- hist(x)
+res
+sum(diff(res$breaks) * res$density)
+
+
+#
+# ─── DATA FRAMES ────────────────────────────────────────────────────────────────
+#
+rm(list = ls())
+dni <- c("22456715A", "22456716B", "22456717C", "22456718D", "22456719E")
+edad <- c(45, 35, 52, 60, 25)
+sexo <- factor(c("Hombre", "Mujer", "Hombre", "Mujer", "Hombre"))
+estudios <- factor(c("superior", "superior", "profesional", "medio", "profesional"))
+salario <- c(2500, 1500, 2000, 1200, 1800)
+
+datos <- data.frame(dni, edad, sexo, estudios, salario)
+datos
+class(datos)
+names(datos)
+str(datos)
+
+summary(datos)
+
+datos[3] # Tratar como lista
+datos[3, ] # Tratar como matriz
+datos[, 3]
+
+datos[1:2, c("edad", "salario")]
+datos[, c(-1, -3, -4)]
+
+model <- lm(datos$salario ~ datos$edad)
+model
+plot(model)
+
+sample(datos, size = 2) # same as
+datos[sample(1:length(datos), 2)]
+
+mtcars
