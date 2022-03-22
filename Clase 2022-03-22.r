91ecea1fb7c9a30274f52a070f9a166896cd50b7
LucasFA <lucaslanzarote@gmail.com>
2022-03-22 13:36:43 +0100

    Apartado de funciones

--- Clase 2022-03-22.r
+++ Clase 2022-03-22.r
@@ -50,7 +50,7 @@ search()
 
 
 # --------------------------------------------------------------------------------
-# Escribit a ficheros
+# Escribir a ficheros
 
 write.table(cars[1:10, ], file = "outputfiles/datos1.txt")
 datos1 <- read.table("datos1.txt", header = TRUE)
@@ -60,3 +60,79 @@ write(t(as.matrix(datos1)), file = "outputfiles/datos3.txt", ncolumns = 2)
 
 write(cars[1:10, ], file = "outputfiles/datos3.txt")
 help("write")
+
+
+#
+# --- FUNCIONES ------------------------------------------------------------------
+#
+
+f.suma <- function(x, y) x + y
+f.suma
+f.suma(1:10, 2)
+f.suma(diag(3), 1:3)
+
+f.suma <- function(x = 0, y = 0) x + y
+
+f2 <- function(x, y) {
+    x^2
+    return(list(suma = x + y, resta = x - y))
+    y^2
+}
+f2(5, 2)
+
+media1 <- function(x) {
+    if (!is.vector(x)) {
+        stop("x no es un vector")
+    }
+    if (!is.numeric(x)) {
+        stop("x no es un vector numérico")
+    }
+    num_na <- sum(is.na(x))
+    media <- sum(x, na.rm = T) / (length(x) - num_na)
+
+    c(media, num_na)
+}
+# Test
+media1(c())
+media1(c(NA))
+media1(c(NA, 1, 3))
+media1(list("o", 1, 3))
+
+media2 <- function(x, n = 1) {
+    if (!is.vector(x) || !is.numeric(x)) {
+        stop("El argumento x no es un vector numérico")
+    }
+    if (!is.vector(n)) {
+        stop("El argumento n no es un vector")
+    }
+    if (any(n < 0)) {
+        warning("El argumento n tiene valores negativos")
+    }
+
+    tam_muestra <- sum(n)
+    media <- drop(x %*% n) / tam_muestra
+
+    list(media = media, tam_muestra = tam_muestra)
+}
+#--------------------------------------------------------------------------------
+# Test
+media2(list(0, 3), c(1, 2))             # not a vector
+media2(unlist(list(0, 3)), c(1, 2))     # a vector
+
+media2(c(0, 3), c(1, 1.777))
+media2(c(0, 1, 5), c(3, 1))
+media2(c(1, 2), c(-11, 1))
+
+# sum(n) = 0 lleva a valores +- inf o NaN
+media2(c(1, 0), c(-1, 1))
+media2(c(0, 1), c(-1, 1))
+
+media2(c(1, 1), c(-1, 1))
+media2(c(-1, -1), c(-1, 1))
+
+media2(c(3), c(2))
+media2(c(), c())
+
+x <- runif(20)
+n <- sample(1:20)
+all.equal(media2(x, n)$media, weighted.mean(x, n))
